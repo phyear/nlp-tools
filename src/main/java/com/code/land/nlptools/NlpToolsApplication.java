3f46e7695f53a0b3d4ca02fe2b1314922f360b6b
zhaotianxin <ztxemail@163.com>
2022-05-28 21:26:05 +0800

    [ADD] init repo

---
+++ src/main/java/com/code/land/nlptools/NlpToolsApplication.java
@@ -0,0 +1,13 @@
+package com.code.land.nlptools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlpToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/code/land/nlptools/NlpToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.land.nlptools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
